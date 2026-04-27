cf553d77dde6a400de56857d4e26ab8299398780
maylia-15 <rabiahriska8@gmail.com>
2026-04-27 22:55:34 +0800

    Delete main directory

--- main/java/com/example/rpl/Parkable.java
+++
@@ -1,5 +0,0 @@
-package com.example.rpl;
-
-public interface Parkable {
-    void park(ParkingSlot slot);
-}
--- main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.rpl {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.rpl to javafx.fxml;
-    exports com.example.rpl;
-}
